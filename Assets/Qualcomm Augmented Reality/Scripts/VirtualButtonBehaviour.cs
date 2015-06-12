eb07aa7837a3c4e349dec84bcec112698ff903aa
bhofmannellis <bhellis@email.arizona.edu>
2015-06-13 00:01:50 +0900

    Removed UnityChan and RobotKyle unused assets

--- Assets/Qualcomm Augmented Reality/Scripts/VirtualButtonBehaviour.cs
+++ Assets/Qualcomm Augmented Reality/Scripts/VirtualButtonBehaviour.cs
@@ -9,42 +9,14 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-using UnityEngine;
-using System.Collections;
-
-public class Custom_VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+namespace Vuforia
 {
-	// Use this for initialization
-	void Start () {
-		
-		// here it finds any VirtualButton Attached to the ImageTarget and register it's event handler and in the
-		//OnButtonPressed and OnButtonReleased methods you can handle different buttons Click state
-		//via "vb.VirtualButtonName" variable and do some really awesome stuff with it.
-		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
-		foreach (VirtualButtonBehaviour item in vbs)
-		{
-			item.RegisterEventHandler(this);
-		}
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	
-	#region VirtualButton
-	
-	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
-	{
-		Debug.Log("Helllllloooooooooo");
-	}
-	
-	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
-	{
-		Debug.Log("Goooooodbyeeee");
-	}
-	
-	#endregion //VirtualButton
+    /// <summary>
+    /// This behaviour associates a Virtual Button with a game object. Use the
+    /// functionality in ImageTargetBehaviour to create and destroy Virtual Buttons
+    /// at run-time.
+    /// </summary>
+    public class VirtualButtonBehaviour : VirtualButtonAbstractBehaviour
+    {
+    }
 }
