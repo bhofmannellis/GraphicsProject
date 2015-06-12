582b6b5acaf8351d19e8a085d2fa248ad163da71
bhofmannellis <bhellis@email.arizona.edu>
2015-06-13 02:45:29 +0900

    Added Buttons and Animations to Ace

---
+++ Assets/VirtualButtonEvent.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Vuforia;
+
+public class VirtualButtonEvent : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject AceModel;
+	private Animation AceAnimations;
+
+	// Use this for initialization
+	void Start () {
+		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour> ();
+		for (int i = 0; i < vbs.Length; i++)
+			vbs [i].RegisterEventHandler(this);
+		AceModel = transform.FindChild ("AceModel").gameObject;
+		AceAnimations = AceModel.GetComponent<Animation> ();
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+		switch (vb.VirtualButtonName){
+		case "AceAttackButton":
+			AceAnimations.Play("AceAttack1");
+			AceAnimations.PlayQueued("AceIdle", QueueMode.CompleteOthers);
+			Debug.Log ("<<<<<AceAttack Pressed>>>>>");
+			break;
+		case "AceSpecialButton":
+			AceAnimations.Play("AceAttack2");
+			AceAnimations.PlayQueued("AceIdle", QueueMode.CompleteOthers);
+			Debug.Log("<<<<<AceSpecial Pressed>>>>>");
+			break;
+		}
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+		if (vb.VirtualButtonName == "AceAttack") {
+			Debug.Log ("<<<<<AceAttack Released>>>>>");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
